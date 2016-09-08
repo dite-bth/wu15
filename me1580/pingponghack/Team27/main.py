d20308a9c43c4cfe4491f9163d7447cd7e95569a
T-bear <torbjorn.holgersson87@gmail.com>
2016-09-08 11:09:43 +0200

    trixar

--- me1580/pingponghack/Team27/main.py
+++ me1580/pingponghack/Team27/main.py
@@ -12,4 +12,4 @@ def registrering_pingponghack():
     return render_templates("registrering_pingponghack.html")
     
 if __name__=="__main__":                                                 #ser till att servern startar när __name__ anropas.
-    app.run (host='0.0.0.0', port = 5000, debug=True)                     #gör den public
+    app.run (debug=True)                     #gör den public
